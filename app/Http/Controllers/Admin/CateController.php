47652883cea16986df0d3ef2054f7718fe5e59e4
lixiaohui <4856284@qq.com>
2017-06-29 23:52:39 +0800

    修改features页面images路径

--- app/Http/Controllers/Admin/CateController.php
+++ app/Http/Controllers/Admin/CateController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 class CateController extends Controller
 {
     public function add()
     {
-        return view('admin.add');
+        if ($input=Input::all()){
+            dd($input);
+           if (DB::table('cate')->insert($input)){
+                echo 11;
+            }else{
+               echo 22;
+           }
+        }else{
+            return view('admin.add');
+        }
+
     }
 }
--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -9,7 +9,7 @@
     <!--面包屑导航 开始-->
     <div class="crumb_warp">
         <!--<i class="fa fa-bell"></i> 欢迎使用登陆网站后台，建站的首选工具。-->
-        <i class="fa fa-home"></i> <a href="#">首页</a> &raquo; <a href="#">商品管理</a> &raquo; 添加商品
+        <i class="fa fa-home"></i> <a href="{{url('admin/index')}}">首页</a> &raquo; 添加商品
     </div>
     <!--面包屑导航 结束-->
 
@@ -29,69 +29,53 @@
     <!--结果集标题与导航组件 结束-->
     
     <div class="result_wrap">
-        <form action="#" method="post">
+        <form action="" method="post">
+            {{csrf_field()}}
             <table class="add_tab">
                 <tbody>
                     <tr>
-                        <th width="120"><i class="require">*</i>分类：</th>
+                        <th><i class="require">*</i>分类名称：</th>
+                        <td>
+                            <input type="text" class="" name="cate_name">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="require">*</i>分类title：</th>
+                        <td>
+                            <input type="text" class="" name="cate_title">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="require">*</i>关键字：</th>
+                        <td>
+                            <input type="text" class="" name="cate_keywords">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="require">*</i>关键字描述：</th>
+                        <td>
+                            <input type="text" class="" name="cate_discription">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="require">*</i>分类排序：</th>
+                        <td>
+                            <input type="text" class="" name="cate_order">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th width="120"><i class="require">*</i>所属父级：</th>
                         <td>
                             <select name="">
                                 <option value="">==请选择==</option>
-                                <option value="19">精品界面</option>
-                                <option value="20">推荐界面</option>
+                                <option value="19" name="cate_pid">精品界面</option>
+                                <option value="" name="cate_pid">推荐界面</option>
                             </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><i class="require">*</i>标题：</th>
-                        <td>
-                            <input type="text" class="lg" name="">
-                            <p>标题可以写30个字</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>作者：</th>
-                        <td>
-                            <input type="text" name="">
-                            <span><i class="fa fa-exclamation-circle yellow"></i>这里是默认长度</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><i class="require">*</i>价格：</th>
-                        <td>
-                            <input type="text" class="sm" name="">元
-                            <span><i class="fa fa-exclamation-circle yellow"></i>这里是短文本长度</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><i class="require">*</i>缩略图：</th>
-                        <td><input type="file" name=""></td>
-                    </tr>
-                    <tr>
-                        <th>单选框：</th>
-                        <td>
-                            <label for=""><input type="radio" name="">单选按钮一</label>
-                            <label for=""><input type="radio" name="">单选按钮二</label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>复选框：</th>
-                        <td>
-                            <label for=""><input type="checkbox" name="">复选框一</label>
-                            <label for=""><input type="checkbox" name="">复选框二</label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>描述：</th>
-                        <td>
-                            <textarea name="discription"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>详细内容：</th>
-                        <td>
-                            <textarea class="lg" name="content"></textarea>
-                            <p>标题可以写30个字</p>
                         </td>
                     </tr>
                     <tr>
