655c861f4e93a7c9ddee3df9dad66ce3e2d2e355
lixiaohui100200 <4856284@qq.com>
2017-07-03 16:29:13 +0800

    添加分类完成

--- app/Http/Controllers/Admin/CateController.php
+++ app/Http/Controllers/Admin/CateController.php
@@ -2,17 +2,38 @@
 
 namespace App\Http\Controllers\Admin;
 
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class CateController extends Controller
 {
     public function add()
     {
         if ($input = Input::except('_token')){
-            dd($input);
+            $rules =[
+                'cate_name' => 'required',
+            ];
+            $message = [
+                'cate_name.required' => '分类名称不允许为空'
+            ];
+
+            $validator = Validator::make($input,$rules,$message);
+            if($validator->passes()){
+                $re = DB::table('cate')->insert($input);
+                if ($re){
+
+                    echo "<script>alert('分类添加成功');location.href='".url('admin/list')."'</script>";
+
+                }else{
+                    echo "<script>alert('分类添加失败');location.href='".url('admin/list')."'</script>";
+                }
+            }else{
+                return back()->withErrors($validator);
+            }
         }else{
             $data = DB::table('cate')->where('cate_pid','0')->select('cate_name','cate_id')->get();
             return view('admin.cate.add')->with('data',$data);
@@ -54,7 +75,7 @@ class CateController extends Controller
         if ($re){
             $data = [
               'state' => '1',
-                'msg' => '排序更新成功'
+                'msg' => '排序更新成功，请手动刷新界面'
             ];
         }else{
             $data = [
--- resources/views/admin/cate/add.blade.php
+++ resources/views/admin/cate/add.blade.php
@@ -17,6 +17,13 @@
 	<div class="result_wrap">
         <div class="result_title">
             <h3>快捷操作</h3>
+            @if (count($errors) > 0)
+                <div class="mark">
+                    @foreach ($errors->all() as $error)
+                        <font color="red"><li>{{ $error }}</li></font>
+                    @endforeach
+                </div>
+            @endif
         </div>
         <div class="result_content">
             <div class="short_wrap">
@@ -27,7 +34,7 @@
         </div>
     </div>
     <!--结果集标题与导航组件 结束-->
-    
+
     <div class="result_wrap">
         <form action="" method="post">
             {{csrf_field()}}
@@ -52,25 +59,25 @@
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>分类标题：</th>
+                        <th>分类标题：</th>
                         <td>
                             <input type="text" class="lg" name="cate_title">
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>关键词：</th>
+                        <th>关键词：</th>
                         <td>
                             <textarea name="cate_keywords"></textarea>
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>关键词描述：</th>
+                        <th>关键词描述：</th>
                         <td>
                             <textarea name="cate_discription"></textarea>
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>分类排序：</th>
+                        <th>分类排序：</th>
                         <td>
                             <input type="text" class="sm" name="cate_order">
                         </td>
